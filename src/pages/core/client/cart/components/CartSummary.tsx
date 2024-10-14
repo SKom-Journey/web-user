3c8392ff50dce0740bebd98484e45889e555e1f6
Melvin-Jovano <melvinjovano2@gmail.com>
2024-10-14 18:01:08 +0700

    UI: cart

--- src/pages/core/client/cart/components/CartSummary.tsx
+++ src/pages/core/client/cart/components/CartSummary.tsx
@@ -1,75 +1,31 @@
-import { EditIcon, LocationDotIcon, MoneyIcon } from "@/components/Icons";
+import { LocationDotIcon, MoneyIcon } from "@/components/Icons";
+import { OrderSummary } from "./OrderSummary";
+import { IMenuOrder } from "@/interfaces/IMenuOrder";
+import getMenu from "@/services/get_menu";
+import { useState } from "react";
 
 export default function CartSummary() {
+    const [orders, setOrders] = useState<IMenuOrder[]>(getMenu());
+
     return(
         <>
             <div className="shadow-lg rounded-lg border p-3 font-bold">
                 <div className="mb-3 text-lg">Order Placed</div>
                 <div className="text-sm flex">
-                <div><LocationDotIcon /></div>
-                <div className="ml-2 font-semibold">Table 12</div>
+                    <div>
+                        <LocationDotIcon />
+                    </div>
+                    <div className="ml-2 font-semibold">Table 12</div>
                 </div>
             </div>
             
-            <div className="shadow-lg rounded-lg border p-3 font-bold mt-5">
-                <div className="mb-3 text-lg">Order Summary</div>
-
-                <div className="text-sm flex flex-wrap font-semibold">
-                <div className="w-full flex">
-                    <div className="w-1/12">2x</div>
-                    <div className="w-full truncate">
-                        Nasi Kandar Mas Rusdi
-                    </div>
-                    <div className="w-4/12 text-right">
-                        Rp.14.000
-                    </div>
-                </div>
-
-                <div className="w-full text-xs text-slate-400 mt-2 flex justify-center items-center">
-                    <div className="mr-1">Note:</div>
-                    <div className="w-full truncate">Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti voluptate odit totam similique impedit commodi error, et quas? Architecto ab ea ipsum debitis illo inventore quaerat optio provident fugit nemo.</div>
-                    <div className="ml-2 text-sm">
-                        <button type="button">
-                            <EditIcon />
-                        </button>
-                    </div>
-                </div>
-                </div>
-
-                <div className="text-sm flex flex-wrap font-semibold mt-5">
-                <div className="w-full flex">
-                    <div className="w-1/12">2x</div>
-                    <div className="w-full truncate">
-                        Nasi Goreng Ngawi Spesial
-                    </div>
-                    <div className="w-4/12 text-right">
-                        Rp.14.000
-                    </div>
-                </div>
-                
-                <div className="w-full text-xs text-slate-400 mt-2 flex justify-center items-center">
-                    <div className="mr-1">Note:</div>
-                    <div className="w-full truncate">-</div>
-                    <div className="ml-2 text-sm">
-                        <button type="button">
-                            <EditIcon />
-                        </button>
-                    </div>
-                </div>
-                </div>
-                
-                <div className="text-lg flex font-bold mt-1 text-green-500 mt-6">
-                <div className="w-full">Subtotal</div>
-                <div className="w-1/12"></div>
-                <div className="w-4/12 text-right">Rp.28.000</div>
-                </div>
-            </div>
+            <OrderSummary setOrders={setOrders} orders={orders} />
 
             <div className="shadow-lg rounded-lg flex border px-3 py-4 justify-center text-lg items-center font-bold mt-5">
                 <div className="w-full">Payment Method</div>
                 <div className="flex justify-center items-center">
-                <MoneyIcon />
-                <div className="ml-2 text-sm">Cash</div>
+                    <MoneyIcon />
+                    <div className="ml-2 text-sm">Cash</div>
                 </div>
             </div>
         </>
---
+++ src/pages/core/client/cart/components/OrderSummary.tsx
@@ -0,0 +1,67 @@
+import { EditIcon } from "@/components/Icons";
+import { IMenuOrder } from "@/interfaces/IMenuOrder";
+import { Dispatch, FC, Fragment, SetStateAction, useEffect } from "react";
+import CartSVG from "@/assets/svg/cart.svg";
+
+interface OrderSummaryComponentProps {
+    setOrders: Dispatch<SetStateAction<IMenuOrder[]>>;
+    orders: IMenuOrder[];
+}
+
+export const OrderSummary: FC<OrderSummaryComponentProps> = ({
+    setOrders,
+    orders
+}) => {
+    useEffect(() => {
+        console.log(orders);
+    }, []);
+
+    if(orders.length === 0) {
+        return (
+            <div className="py-8 shadow-lg rounded-lg border p-3 font-bold mt-5 flex flex-wrap items-center justify-center">
+                <img className="w-1/2" src={CartSVG} alt="" />
+                <center className="mt-4 text-lg w-full">No orders found yet...</center>
+            </div>
+        )
+    }
+
+    return (
+        <div className="shadow-lg rounded-lg border p-3 font-bold mt-5">
+            <div className="mb-3 text-lg">Order Summary</div>
+
+            <div className="text-sm flex flex-wrap font-semibold">
+                {
+                    orders.map((o, i) => (
+                        <Fragment key={i}>
+                            <div className="w-full flex">
+                                <div className="w-1/12">{o.total}x</div>
+                                <div className="w-full truncate">
+                                    {o.title}
+                                </div>
+                                <div className="w-4/12 text-right">
+                                    Rp.{o.price}
+                                </div>
+                            </div>
+
+                            <div className="w-full text-xs text-slate-400 mt-2 flex justify-center items-center">
+                                <div className="mr-1">Note:</div>
+                                <div className="w-full truncate">{o.note.length > 0 ? o.note : '-'}</div>
+                                <div className="ml-2 text-sm">
+                                    <button type="button">
+                                        <EditIcon />
+                                    </button>
+                                </div>
+                            </div>
+                        </Fragment>
+                    ))
+                }
+            </div>
+
+            <div className="text-lg flex font-bold mt-1 text-green-500 mt-6">
+                <div className="w-full">Subtotal</div>
+                <div className="w-1/12"></div>
+                <div className="w-4/12 text-right">Rp.28.000</div>
+            </div>
+        </div>
+    )
+}
